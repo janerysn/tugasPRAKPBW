d35ab198ed368689547b566aab7ef71014330602
zahra <zahrajanearnc@gmail.com>
2024-10-11 11:20:54 +0700

    prak06

---
+++ belajar_laravel/resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title>Selamat Datang</title>
+</head>
+<body>
+    <h1>Selamat Datang di Buku Tamu</h1>
+    <a href="{{ route('bukutamu.create') }}">Isi Buku Tamu</a> |
+    <a href="{{ route('bukutamu.show') }}">Lihat Komentar</a>
+</body>
+</html>
